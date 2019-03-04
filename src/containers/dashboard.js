2663909113f10603e7f23fe6b348026c0dcba674
Ankur Anant <ankuranant99@gmail.com>
2019-03-04 23:45:57 +0530

    url change

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -67,7 +67,7 @@ class Dashboard extends Component {
                                 <h1 className="text-white text-center mt-3">User Page</h1>
                                 <p className="text-white text-center">This page will eventually contain your favourite/bookmarked resources and various user options, if you logged in by authenticating with Discord</p>
                                 <p >
-                                    <a href="https://discordapp.com/api/oauth2/authorize?client_id=520955050793893891&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fuser%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify%20guilds">login</a>
+                                    <a href="https://discordapp.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fdev-resources.herokuapp.com%2Fuser%2Fauth%2Fdiscord%2Fcallback&scope=identify%20guilds">login</a>
                                 </p>
                             </div>
                 }
