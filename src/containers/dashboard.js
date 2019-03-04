a87a9dbc9b741e42eebee9fb2710f9d757f3b7e5
Matt Smith <mattchow86@gmail.com>
2019-03-04 20:45:59 +0000

    Update dashboard.js

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -67,7 +67,7 @@ class Dashboard extends Component {
                                 <h1 className="text-white text-center mt-3">User Page</h1>
                                 <p className="text-white text-center">This page will eventually contain your favourite/bookmarked resources and various user options, if you logged in by authenticating with Discord</p>
                                 <p >
-                                    <a href="https://discordapp.com/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fdev-resources.herokuapp.com%2Fuser%2Fauth%2Fdiscord%2Fcallback&scope=identify%20guilds">login</a>
+                                    <a href=`https://discordapp.com/api/oauth2/authorize?client_id=537744904940683277&redirect_uri=https://rustyresources.herokuapp.com/user/auth/discord/callback&response_type=code&scope=identify%20guilds`>login</a>
                                 </p>
                             </div>
                 }
